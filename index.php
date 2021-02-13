c48e0b582dfb35d871e8e5ebf2825fb24a20347c
boubasano <48012386+boubasano@users.noreply.github.com>
2021-02-14 00:46:02 +0100

    social media buttons

--- index.php
+++ index.php
@@ -53,7 +53,9 @@
     </footer>
 </main>
 
-
+<script type="text/javascript"
+        src="https://platform-api.sharethis.com/js/sharethis.js#property=6028628f0cf79300112b0f37&product=inline-share-buttons"
+        async="async"></script>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
         integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
         crossorigin="anonymous"></script>
--- templates/contact/contact.php
+++ templates/contact/contact.php
@@ -16,45 +16,46 @@
         </div>
 
         <div class="social-button">
-            <div>
-                <!-- Facebook -->
-                <a target="_blank" title="Facebook"
-                   href="https://www.facebook.com/sharer.php?u=https://tontonduweb.com/previews-warc/genieCivil/article1.html"
-                   rel="nofollow"
-                   onclick="window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=500,width=700');return false;"><img
-                            src="iconrs/facebook_icon.png" alt="Facebook"/></a>
-                <!-- //Facebook -->
-
-                <!-- Twitter -->
-                <a target="_blank" title="Twitter"
-                   href="https://twitter.com/share?url=https://bit.ly/2sI7H3v"
-                   rel="nofollow"
-                   onclick="window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=400,width=700');return false;"><img
-                            src="iconrs/twitter_icon.png" alt="Twitter"/></a>
-                <!-- //Twitter -->
-
-                <!-- Google + -->
-                <a target="_blank" title="Google +"
-                   href="https://plus.google.com/share?url=https://tontonduweb.com/previews-warc/genieCivil/article1.html"
-                   rel="nofollow"
-                   onclick="window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=450,width=650');return false;"><img
-                            src="iconrs/gplus_icon.png" alt="Google Plus"/></a>
-                <!-- //Google + -->
-
-                <!-- Linkedin -->
-                <a target="_blank" title="Linkedin"
-                   href="https://www.linkedin.com/shareArticle?mini=true&amp;url=https://tontonduweb.com/previews-warc/genieCivil/article1.html"
-                   rel="nofollow"
-                   onclick="window.open(this.href, '','menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=450,width=650');return false;"><img
-                            src="iconrs/linkedin_icon.png" alt="Linkedin"/></a>
-                <!-- //Linkedin -->
-
-                <!-- Email -->
-                <a target="_blank" title="Envoyer par mail"
-                   href="mailto:?Subject=Regarde ça c'est cool !&amp;Body=regarde%20cet%20article%20c'est%20super !%20 https://tontonduweb.com/previews-warc/genieCivil/article1.html"
-                   rel="nofollow"><img src="iconrs/email_icon.png" alt="email"/></a>
-                <!-- //Email -->
-            </div>
+<!--            <div>-->
+<!--               Facebook -->
+<!--                <a target="_blank" title="Facebook"-->
+<!--                   href="https://www.facebook.com/sharer.php?u=https://aboubacarsano.com"-->
+<!--                   rel="nofollow"-->
+<!--                   onclick="window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=500,width=700');return false;"><img-->
+<!--                            src="iconrs/facebook_icon.png" alt="Facebook"/></a>-->
+<!--                 //Facebook -->
+<!---->
+<!--                 Twitter -->
+<!--                <a target="_blank" title="Twitter"-->
+<!--                   href="https://twitter.com/share?url=https://aboubacarsano.com"-->
+<!--                   rel="nofollow"-->
+<!--                   onclick="window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=400,width=700');return false;"><img-->
+<!--                            src="iconrs/twitter_icon.png" alt="Twitter"/></a>-->
+<!--                //Twitter -->
+<!---->
+<!--                Google + -->
+<!--                <a target="_blank" title="Google +"-->
+<!--                   href="https://plus.google.com/share?url=https://aboubacarsano.com"-->
+<!--                   rel="nofollow"-->
+<!--                   onclick="window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=450,width=650');return false;"><img-->
+<!--                            src="iconrs/gplus_icon.png" alt="Google Plus"/></a>-->
+<!--                //Google + -->
+<!---->
+<!--               Linkedin -->
+<!--                <a target="_blank" title="Linkedin"-->
+<!--                   href="https://www.linkedin.com/shareArticle?mini=true&amp;url=https://aboubacarsano.com"-->
+<!--                   rel="nofollow"-->
+<!--                   onclick="window.open(this.href, '','menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=450,width=650');return false;"><img-->
+<!--                            src="iconrs/linkedin_icon.png" alt="Linkedin"/></a>-->
+<!--                //Linkedin -->
+<!---->
+<!--                 Email -->
+<!--                <a target="_blank" title="Envoyer par mail"-->
+<!--                   href="mailto:?Subject= Merci pour le partage&amp;Body=regarde%20ce%20site%20cà%20devrait %20t'intéresser ! %20https://aboubacarsano.com"-->
+<!--                   rel="nofollow"><img src="iconrs/email_icon.png" alt="email"/></a>-->
+<!--                 //Email --->
+<!--            </div>-->
+            <div class="sharethis-inline-share-buttons"></div>
         </div>
     </div>
 
